6b97eb4b06a1bb8cde5204c965c4c173495695bb
marvinex <133092122+marvinex@users.noreply.github.com>
2023-05-31 20:54:47 +0200

    Generation 2

---
+++ whats-app/src/Login.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefautl();
+        console.log(email);
+    }
+
+    return(
+        <div className="auto-form-container">
+            <h2>Login</h2>
+        <form className="login-form" onSubmit={handleSubmit}>
+            <label htmlFor="email">Number</label>
+            <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="+49 1234 567890" id="email" name="email"/>
+            <label htmlFor="password">Password</label>
+            <input value={pass} onChange={(e)=>setPass(e.target.value)} type="password" placeholder="*********" id="password" name="password"/>
+            <button type="submit">Log In</button>
+        </form>
+        <button className="link-btn" onClick={()=> props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
+}
