82115791d522de777febd4cadaeb152ac52b2092
Leonit Mazrekaj <leonit.mazrekaj@sap.com>
2023-05-31 20:45:18 +0200

    Generation

--- Frontend/whatsapp/src/App.js
+++ Frontend/whatsapp/src/App.js
@@ -1,20 +1,21 @@
-import React,{useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import { Login } from "./Login";
 import { Register } from "./Register";
 
-
 function App() {
-  const [currentForm, setCurrentForm] = useState('login')
+  const [currentForm, setCurrentForm] = useState("login");
 
-  const toggleForm = (formName) =>{
+  const toggleForm = (formName) => {
     setCurrentForm(formName);
-  }
+  };
   return (
     <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
-      }
+      {currentForm === "login" ? (
+        <Login onFormSwitch={toggleForm} />
+      ) : (
+        <Register onFormSwitch={toggleForm} />
+      )}
     </div>
   );
 }
